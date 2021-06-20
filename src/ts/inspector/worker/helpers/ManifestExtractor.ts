07acb5019b879e9657e2d90cab4617c0ac6e646f
Tim Weißenfels <tim-we@users.noreply.github.com>
2021-06-20 16:07:20 +0200

    Add manifest tag

--- src/ts/inspector/worker/helpers/ManifestExtractor.ts
+++ src/ts/inspector/worker/helpers/ManifestExtractor.ts
@@ -2,10 +2,17 @@ import { Manifest } from "../../../types/Manifest";
 import * as zip from "@zip.js/zip.js";
 import { TreeFile, TreeFolder } from "../FileTree";
 
-export async function getManifest(root: TreeFolder): Promise<Manifest> {
+export async function getManifest(
+    root: TreeFolder,
+    tag: boolean = true
+): Promise<Manifest> {
     const manifestNode = root.get("manifest.json") as TreeFile;
     if (!(manifestNode instanceof TreeFile)) {
         throw new Error("Could not find manifest.json.");
+    }
+
+    if (tag) {
+        manifestNode.addTag("manifest");
     }
 
     const text: string = await manifestNode.entry.getData!(
