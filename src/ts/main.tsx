4e03dab6e5b3766b4c26fdc12ae808f896f3cea7
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2022-02-27 20:37:45 +0100

    Switch router (#17)
    * use wouter (incomplete)
    
    * Rewrite ExtensionSelector with hooks
    
    * Base path
    
    * Fix local files
    
    Co-authored-by: Tim Weißenfels <tim-we@users.noreply.github.com>

--- src/ts/main.tsx
+++ src/ts/main.tsx
@@ -1,5 +1,5 @@
 import * as Preact from "preact";
-import { Route, Router } from "preact-router";
+import { Router, Link, Switch, Route, useLocation } from "wouter-preact";
 
 import ExtensionInspector from "./components/ExtensionInspector";
 import ExtensionSelector from "./components/ExtensionSelector";
@@ -10,64 +10,84 @@ import * as LFP from "./utils/LocalFileProvider";
 // create styles (in <head>)
 import "prismjs/themes/prism-okaidia.css";
 import "../less/app.less";
-import { AppLink, customRoute } from "./utils/Routing";
 
-type ExtInspectorFC = Preact.FunctionalComponent<{ id: string }>;
+const pathBase = window.location.host.endsWith("github.io")
+    ? "/web-ext-inspector"
+    : undefined;
 
-const FirefoxExtensionInspector: ExtInspectorFC = ({ id }) => (
-    <ExtensionInspector extension={{ type: "amo", id }} />
-);
-const ChromeExtensionInspector: ExtInspectorFC = ({ id }) => (
-    <ExtensionInspector extension={{ type: "cws", id }} />
-);
-const LocalExtensionInspector: ExtInspectorFC = ({ id }) => {
-    const url = LFP.getURL(id);
-    if (!url) {
-        customRoute("/", true);
-        return null;
+const App: Preact.FunctionalComponent = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const navigate = useLocation()[1];
+
+    if (urlParams.has("route")) {
+        console.log("routing to " + urlParams.get("route"));
+        navigate(urlParams.get("route")!, { replace: true });
+    } else if (urlParams.has("extension")) {
+        if (/^[a-z0-9\-]+$/.test(urlParams.get("extension")!)) {
+            const sources = new Map([
+                ["amo", "firefox"],
+                ["cws", "chrome"],
+            ]);
+            const store = urlParams.get("store");
+            const source =
+                store && sources.has(store) ? sources.get(store) : "firefox";
+            const route =
+                "/inspect/" + source + "/" + urlParams.get("extension")!;
+            navigate(route, { replace: true });
+        }
     }
-    return <ExtensionInspector extension={{ type: "url", url }} />;
+
+    return (
+        <Router base={pathBase}>
+            <header>
+                <Link href="/">
+                    <h1>Extension Inspector</h1>
+                </Link>
+            </header>
+            <Switch>
+                <Route path="/inspect/firefox/:id">
+                    {({ id }) => (
+                        <ExtensionInspector extension={{ type: "amo", id }} />
+                    )}
+                </Route>
+                <Route path="/inspect/chrome/:id">
+                    {({ id }) => (
+                        <ExtensionInspector extension={{ type: "cws", id }} />
+                    )}
+                </Route>
+                <Route path="/inspect/file/:id">
+                    {({ id }) => {
+                        const url = LFP.getURL(id);
+                        const navigate = useLocation()[1];
+                        if (!url) {
+                            navigate("/", { replace: true });
+                            return null;
+                        }
+                        return (
+                            <ExtensionInspector
+                                extension={{ type: "url", url }}
+                            />
+                        );
+                    }}
+                </Route>
+                <Route>
+                    <ExtensionSelector />
+                </Route>
+            </Switch>
+            <div class="hfill"></div>
+            <footer>
+                <a
+                    id="view-on-github"
+                    href="https://github.com/tim-we/web-ext-inspector"
+                    data-native
+                >
+                    View on GitHub
+                </a>
+            </footer>
+            <ConfigUI />
+        </Router>
+    );
 };
-
-class App extends Preact.Component<{}> {
-    public render() {
-        return (
-            <>
-                <header>
-                    <AppLink href="/">
-                        <h1>Extension Inspector</h1>
-                    </AppLink>
-                </header>
-                <Router>
-                    <Route
-                        path="/inspect/firefox/:id"
-                        component={FirefoxExtensionInspector}
-                    />
-                    <Route
-                        path="/inspect/chrome/:id"
-                        component={ChromeExtensionInspector}
-                    />
-                    <Route
-                        path="/inspect/file/:id"
-                        component={LocalExtensionInspector}
-                    />
-                    <Route path="/" component={ExtensionSelector} default />
-                </Router>
-                <div class="hfill"></div>
-                <footer>
-                    <a
-                        id="view-on-github"
-                        href="https://github.com/tim-we/web-ext-inspector"
-                        data-native
-                    >
-                        View on GitHub
-                    </a>
-                </footer>
-                <ConfigUI />
-            </>
-        );
-    }
-}
 
 const root = document.createElement("div");
 root.id = "root";
@@ -79,24 +99,3 @@ document.body.appendChild(modalPortal);
 setPortal(modalPortal);
 
 Preact.render(<App />, root);
-
-(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has("route")) {
-        console.log("routing to " + urlParams.get("route"));
-        customRoute(urlParams.get("route")!, true);
-    } else if (urlParams.has("extension")) {
-        if (!/^[a-z0-9\-]+$/.test(urlParams.get("extension")!)) {
-            return;
-        }
-        const sources = new Map([
-            ["amo", "firefox"],
-            ["cws", "chrome"],
-        ]);
-        const store = urlParams.get("store");
-        const source =
-            store && sources.has(store) ? sources.get(store) : "firefox";
-        const r = "/inspect/" + source + "/" + urlParams.get("extension")!;
-        customRoute(r, true);
-    }
-})();
